2a150380f6099b1d2e8df6a1ada48397c0b60dcb
milovan-minic <milovan.minic@gmail.com>
2016-05-20 13:04:46 +0200

    Task 04 - NOT Finished
    There is a problem when passing value for changed timezone as a parameter.

---
+++ task04.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * ### Task 4
+ * Write a PHP script to display time in a specified timezone.
+ */
+
+$europeTimezones = array('Europe/Amsterdam', 'Europe/Andorra', 'Europe/Astrakhan',
+    'Europe/Athens', 'Europe/Belgrade', 'Europe/Berlin', 'Europe/Bratislava',
+    'Europe/Brussels', 'Europe/Bucharest', 'Europe/Budapest', 'Europe/Busingen',
+    'Europe/Chisinau', 'Europe/Copenhagen', 'Europe/Dublin', 'Europe/Gibraltar',
+    'Europe/Guernsey', 'Europe/Helsinki', 'Europe/Isle_of_Man', 'Europe/Istanbul',
+    'Europe/Jersey', 'Europe/Kaliningrad', 'Europe/Kiev', 'Europe/Kirov',
+    'Europe/Lisbon', 'Europe/Ljubljana', 'Europe/London', 'Europe/Luxembourg',
+    'Europe/Madrid', 'Europe/Malta', 'Europe/Mariehamn', 'Europe/Minsk',
+    'Europe/Monaco', 'Europe/Moscow', 'Europe/Oslo', 'Europe/Paris',
+    'Europe/Podgorica', 'Europe/Prague', 'Europe/Riga', 'Europe/Rome',
+    'Europe/Samara', 'Europe/San_Marino', 'Europe/Sarajevo', 'Europe/Simferopol',
+    'Europe/Skopje', 'Europe/Sofia', 'Europe/Stockholm', 'Europe/Tallinn',
+    'Europe/Tirane', 'Europe/Ulyanovsk', 'Europe/Uzhgorod', 'Europe/Vaduz',
+    'Europe/Vatican', 'Europe/Vienna', 'Europe/Vilnius', 'Europe/Volgograd',
+    'Europe/Warsaw', 'Europe/Zagreb', 'Europe/Zaporozhye', 'Europe/Zurich',
+    'Antarctica/Casey', 'Antarctica/Davis');
+
+
+if(isset($_POST['submit'])) {
+    $tz = $_POST['tz'];
+    var_dump($tz);
+
+    $time = new DateTime();
+    $time->setTimezone($tz);
+
+    echo 'Time in ' . $tz . ' is ';
+    echo date_format($time, 'H:i:s') . PHP_EOL;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Time</title>
+</head>
+
+<body>
+<h3>Pick timezone</h3>
+<form name="timezone_form" action="task04.php" method="post">
+    <select name="tz">
+        <?php
+        foreach($europeTimezones as $timeZone) {
+            echo '<option value="' . $timeZone . '">' . $timeZone . '</option>';
+        }
+        ?>
+    </select>
+    <input type="submit" name="submit" value="Display">
+</form>
+</body>
+</html>
